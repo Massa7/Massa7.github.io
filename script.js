e69064f83089bc5b7bd8da632a576b14666d2e02
massa7 <68928030+massa7@users.noreply.github.com>
2020-07-28 21:59:59 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+﻿
+// WEATHER from https://weatherwidget.io/
+!function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0]; if (!d.getElementById(id)) {
+        js = d.createElement(s); js.id = id; js.src = 'https://weatherwidget.io/js/widget.min.js'; fjs.parentNode.insertBefore(js, fjs);
+    }
+}(document, 'script', 'weatherwidget-io-js');
+
+// display clock
+function showTime() {
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+
+    setTimeout(showTime, 1000);
+
+}
+
+showTime();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
